024f1e9608fee5f3895edfd0606c9a3b14ca7668
Aleksei Paimetov <Aleksei_Paimetov@epam.com>
2019-07-05 14:28:44 +0400

    Where gitignore?

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class User
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+            User n = new User();
+            Console.WriteLine(n.X);
+
+        }
+    }
+}
